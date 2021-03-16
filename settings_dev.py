ecdc6d19abd0d6903b302304600a40d18c58d506
Ajibson <azeezajibson123@gmail.com>
2021-03-16 09:57:31 +0100

    delete setting_dev

--- settings_dev.py
+++
@@ -1,7 +0,0 @@
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'myvtuservice@gmail.com'
-EMAIL_HOST_PASSWORD = 'haxdcordowsaonhv'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FROM_MAIL = 'myvtuservice@gmail.com'
